abbbef2cf4d336a9393711c9eccd3149b7f7872d
osmng <60536349+DenisLee020205@users.noreply.github.com>
2022-05-13 16:22:33 +0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+function show() {
+	document.getElementById('1').style.display='inline';
+	document.getElementById('2').style.display='inline';
+}
+function hide() {
+	document.getElementById('1').style.display='none';
+	document.getElementById('2').style.display='none';
+}
